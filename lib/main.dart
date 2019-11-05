64f2312088ae19d6b4695bd6a8250980cd05b7f5
WorrawutK <worrawutk@gmail.com>
2019-11-05 15:47:01 +0700

    Getdata model to page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wutrist/screens/home.dart';
 
 void main() {
-  runApp(MyApp()); // Ctrl + .
+  runApp(MyApp()); // Ctrl + .  //Ctrl + Shft + F
 }
 
 class MyApp extends StatelessWidget {
--- lib/widget/page1.dart
+++ lib/widget/page1.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:wutrist/models/pageholder_model.dart';
 
 class Page1 extends StatefulWidget {
   @override
@@ -6,10 +10,52 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+// Explicit
+  List<String> str = ['xx', 'xcs', 'yy']; //example
+  List<PageHolderModel> pageHolderModel = [];
+
+// Method
+  @override
+  void initState() {
+    super.initState();
+    readAllData();
+  }
+
+  Future<void> readAllData() async {
+    String urlAPI = 'https://jsonplaceholder.typicode.com/posts';
+    Response response = await get(urlAPI);
+    var result = json.decode(response.body);
+    print('result $result');
+
+    for (var map in result) {
+      PageHolderModel holderModel = PageHolderModel.fromJson(map);
+      String title = holderModel.title;
+      print('title = $title');
+      setState(() {
+        pageHolderModel.add(holderModel);
+      });
+    }
+  }
+
+  Widget showTite(int index) {
+    return Text(pageHolderModel[index].title);
+  }
+
+  Widget showListView(int index) {
+    return Container(decoration: index%2 == 0 ? BoxDecoration(color: Colors.blue):BoxDecoration(color: Colors.blue[100]),
+      child: Column(
+        children: <Widget>[showTite(index),],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Page 1'),
+    return ListView.builder(
+      itemCount: pageHolderModel.length,
+      itemBuilder: (BuildContext context, int index) {
+        return showListView(index);//Text(pageHolderModel[index].title);
+      },
     );
   }
 }
